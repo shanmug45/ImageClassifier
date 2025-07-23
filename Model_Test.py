a586546e383103e3ddc760fb6510cf3cf4bf83aa
Shanmugasundaram Thangarasu <101005284+shanmug45@users.noreply.github.com>
2025-07-23 22:26:55 +0100

    Create Model_Test.py

---
+++ Model_Test.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+
+img_height = 150
+img_width = 150
+
+loaded_model = tf.keras.models.load_model('model_P3.h5')
+
+# Define the path to your single image file
+img_path = '/content/Web_Photo_Editor.jpg' # Replace with the actual path to your image
+
+# Load the image and resize it
+img = image.load_img(img_path, target_size=(img_height, img_width))
+
+# Convert the image to a numpy array
+img_array = image.img_to_array(img)
+
+# Add a batch dimension
+img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+# Make the prediction
+predictions = loaded_model.predict(img_array)
+
+# Get the predicted class (0 or 1)
+predicted_class = tf.round(predictions[0]).numpy().item()
+
+print(f"The predicted class for the image is: {predicted_class}")
